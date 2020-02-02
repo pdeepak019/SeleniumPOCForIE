fc40a94831ec37440460c8d1a20591332f32d9e7
Deepak <deepak.invincible28@gmail.com>
2020-02-02 23:48:47 +0530

    Initial

--- src/main/java/com/deepak/pocs/stepdefinition/Steps.java
+++ src/main/java/com/deepak/pocs/stepdefinition/Steps.java
@@ -7,30 +7,36 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.sikuli.script.Screen;
 
-public class Steps {				
+public class Steps {
 
-    WebDriver driver;			
-    		
+    WebDriver driver;
+
     @Given("^Open the IE and launch the application$")
-    public void open_the_Firefox_and_launch_the_application() throws Throwable							
-    {		
-       System.setProperty("webdriver.ie.driver", "C://Users//Admin//Downloads//IE//IEDriverServer.exe");					
-       driver= new InternetExplorerDriver();					
-       driver.manage().window().maximize();			
-       driver.get("http://google.com");					
-    }		
+    public void open_the_Firefox_and_launch_the_application() throws Throwable
+    {
+       System.setProperty("webdriver.ie.driver", "C://Users//Admin//Downloads//IE//IEDriverServer.exe");
+       driver= new InternetExplorerDriver();
+       driver.manage().window().maximize();
+        driver.get("https://www.youtube.com/?gl=IN");
+        Screen sc = new Screen();
+        sc.click();
+        sc.type("C:\\Users\\Admin\\Desktop\\search.PNG", "eTheRealYT");
+        sc.click("C:\\Users\\Admin\\Desktop\\submit.PNG");
+
+    }
 
     @When("^Enter the Username and Password$")
-    public void enter_the_Username_and_Password() throws Throwable 							
-    {		
-       driver.findElement(By.name("uid")).sendKeys("username12");							
-       driver.findElement(By.name("password")).sendKeys("password12");							
-    }		
+    public void enter_the_Username_and_Password() throws Throwable
+    {
+       driver.findElement(By.name("uid")).sendKeys("username12");
+       driver.findElement(By.name("password")).sendKeys("password12");
+    }
 
     @Then("^Reset the credential$")
-    public void Reset_the_credential() throws Throwable 							
-    {		
-       driver.findElement(By.name("btnReset")).click();					
-    }		
+    public void Reset_the_credential() throws Throwable
+    {
+       driver.findElement(By.name("btnReset")).click();
+    }
 }
